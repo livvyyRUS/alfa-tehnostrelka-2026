6773ffd3775c6f813699ed72236312f1af4192a2
livvyyRUS <rem.game.on@gmail.com>
2026-04-27 13:15:14 +0300

    add_promts

--- agents/system_messages.py
+++ agents/system_messages.py
@@ -1,3 +1,14 @@
+import os
+from pathlib import Path
+
 system_messages = {
     
 }
+
+for prompt_file in os.listdir("promts"):
+    path = Path("promts", prompt_file)
+    with open(path, "r", encoding="utf8") as file:
+        prompt = file.read()
+        system_messages[path.stem] = prompt
+
+print(system_messages.get("code_gen"))
